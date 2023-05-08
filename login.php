5b105d2ce1c9253b33613c21657dc39f8db2ec60
MorphyKutay <kutaynevzat2006@gmail.com>
2023-05-08 23:51:34 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,54 +1,29 @@
 <?php
 
 session_start();
-
+$_SESSION['id'] = $id;
 include "baglan.php";
 
-if(isset($_POST['uname']) && isset($_POST['password']))
-{
-	function validate($data)
-	{
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-	$uname = validate($_POST['uname']);
-	$pass = validate($_POST['password']);
+// Formdan kullanıcı adı ve şifreyi al
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $kullanici_adi = $_POST['uname'];
+    $sifre = $_POST['password'];
 
-	if(empty($uname))
-	{
-		header("Location: admin.php?error=username gerekli");
-		exit();
-	}
-	else if (empty($pass))
-	{
-		header("Location: admin.php?error=sifre gerekli");
-		exit();
-	}
-	else{
-		$sql = "SELECT * FROM veri WHERE uname='$uname' AND pass='$pass'";
-		$result = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($result) == 1)
-		{
-			$row = mysqli_fetch_assoc($result);
-			if($row['uname'] === $uname&& $row['pass']=== $pass)
-			{
-				header("Location: home.php");
-				exit();
-			}/*else{
-				header("Location: admin.php?error=Yanlis Kullanici Adi Veya Sifre");
-				exit();
-			}*/
-		}else
-		{
-			header("Location: admin.php?error=Yanlis Kullanici Adi Veya Sifre");
-			exit();
-		}
+    // Veritabanından kullanıcıyı sorgula
+    $sql = "SELECT * FROM veri WHERE uname = '$kullanici_adi' AND pass = '$sifre'";
+    $result = $conn->query($sql);
 
-	}
-}else{
-	header("Location: admin.php");
-	exit();
+    if ($result->num_rows > 0) {
+        // Oturumu başlat ve kullanıcı bilgilerini sakla
+        $_SESSION["kullanici_adi"] = $kullanici_adi;
+        $_SESSION["giris_zamani"] = time(); // Unix zaman damgası
+        $_SESSION['adminlogin'] = true;
+       
+        // Ana sayfaya yönlendir
+        header("Location: home.php");
+        exit();
+    } else {
+        echo "hata";
+    }
 }
 ?>
